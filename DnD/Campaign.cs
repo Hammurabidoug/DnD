3b87bc5a74b9be07cf8b83e4c4f8f6819e9c71ca
Hammurabidoug <hammurabidoug@gmail.com>
2020-01-21 20:20:29 -0500

    Initial commit

---
+++ DnD/Campaign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DnD
+{
+    public class Campaign
+    {
+        public Campaign(string name)
+        {
+            this.Name = name;
+        }
+
+        public object Name { get; }
+    }
+}
