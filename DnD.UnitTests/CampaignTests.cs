040b7ff0f4a3f0d726b94725c1aa81f5724aa73e
Hammurabidoug <hammurabidoug@gmail.com>
2020-01-21 20:45:43 -0500

    Adds unit tests

---
+++ DnD.UnitTests/CampaignTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DnD.UnitTests
+{
+    public class CampaignTests
+    {
+        [Fact]
+        public async Task CreateCampaign()
+        {
+            var campaignName = Guid.NewGuid().ToString();
+            var campaign = await Task.FromResult(new Campaign(campaignName));
+
+            Assert.NotNull(campaign.Name);
+        }
+
+        [Fact]
+        public async Task NamePropertyIsSetOnCreate()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var campaign = await Task.FromResult(new Campaign(expected));
+
+            var actual = campaign.Name;
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
